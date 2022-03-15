708b156f8157690b7b46a52e68d6ca89c183ac34
tleopardt <rizalrezx66@gmail.com>
2022-03-15 10:48:44 +0700

    ss

--- src/component/CardItem/index.js
+++ src/component/CardItem/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment } from 'react'
 import { DollarFormat } from '../../utils/DollarFormat'
 import DateFormat from '../../utils/DateFormat'
 import './style.css'
